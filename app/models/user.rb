ccebe2e9e5767131c6f1f4872adbebe404fcf1c3
Abraham0912 <09120912ahn@gmail.com>
2021-11-25 15:48:52 -0600

    relation ship and validate unique

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :links
+
+  validates_uniqueness_of :email
          devise :database_authenticatable,
          :jwt_authenticatable,
          :registerable,
