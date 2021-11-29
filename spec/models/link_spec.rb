a5083d29307100fb2b4b49a5dadd76cf2d671778
Abraham0912 <09120912ahn@gmail.com>
2021-11-29 12:10:20 -0600

    test link model in proces...

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -4,4 +4,23 @@ RSpec.describe Link do
     it "Deberia existir una clase de nombre Link" do
         
     end
+
+    it "Deberia validar que existe un url campo" do
+        value = Link.new(url: nil)
+        expect(value).to_not be_valid
+    end
+
+    it "Deberia validar que code campo es unico" do
+        Link.create(code: "123456")
+        value = Link.new(url: nil)
+        expect(value).to_not be_valid
+    end
+
+    it "Deberia validar que code campo es tamaño 6" do
+        # your code
+    end
+
+    it "Deberia validar que url campo tiene un formato html || htmls" do
+        # your code
+    end
 end
